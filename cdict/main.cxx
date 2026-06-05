90faffd2387d53388ab5224fa13b6c2a0ed78177
Kirill Diduk <kirill.diduk@gmail.com>
2026-06-06 01:06:59 +0200

    Add comparison with std::map

--- cdict/main.cxx
+++ cdict/main.cxx
@@ -3,7 +3,9 @@
 #include <cstring>
 
 #include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <map>
 #include <random>
 #include <stdexcept>
 #include <string>
@@ -25,6 +27,7 @@ private:
     const size_t query_count;
 
     struct ::dict* cdict = nullptr;
+    std::map<std::string, int> rbtree;
     std::unordered_map<std::string, int> hashmap;
     
     // all keys in a string form, all with the same length
@@ -89,6 +92,15 @@ public:
         for (int i = 0; i < dict_size; ++i) {
             dict_put(cdict, keys[i].c_str(), i);
             hashmap[keys[i]] = i;
+            rbtree[keys[i]] = i;
+        }
+
+        if (hashmap.size() != dict_size) {
+            throw std::runtime_error("std::unordered map not initialized correctly");
+        }
+    
+        if (rbtree.size() != dict_size) {
+            throw std::runtime_error("std::map map not initialized correctly");
         }
     }
 
@@ -116,6 +128,23 @@ public:
         return std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
     }
 
+    long long benchmark_map(size_t runs_count) const
+    {
+        using timer = std::chrono::steady_clock;
+
+        volatile long long sink = 0;
+
+        timer::time_point begin = timer::now();
+        for (size_t i = 0; i < runs_count; ++i) {
+            const std::string& key = keys[queries[i]];
+            auto it = rbtree.find(key);
+            sink += it->second;
+        }
+        timer::time_point end = timer::now();
+        
+        return std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+    }
+
     long long benchmark_unordered_map(size_t runs_count) const
     {
         using timer = std::chrono::steady_clock;
@@ -135,7 +164,7 @@ public:
 };
 
 
-int main(int argc, char** argv)
+int main()
 {
     std::cout << "Benchmarking of naive dictionary implementation in C"
         #ifdef NDEBUG
@@ -143,36 +172,68 @@ int main(int argc, char** argv)
         #else
             << " [Debug build]\n"
         #endif
-        << "Compare brute force array-based dictionary vs std::unordered_map\n\n"
+        << "Compare performance of the following variants:\n"
+        << " - simple but naive array-based dictionary in plain C with full search\n"
+        << " - std::map from C++\n"
+        << " - std::unordered_map from C++\n\n"
         << "Parameters: \n"
         << "N = size of the dictionary (number of items in the dictionary)\n"
         << "L = lengt of the keys (we use keys of the equal length)\n"
         << "Q = number of find queries to the dictionary (we search only existing keys)\n"
+        << "R = number of repeated runs (we use the best of them)\n"
+        << "=====================================================\n"
         << std::endl;
 
     const std::initializer_list<size_t> dict_sizes = { 10, 20, 50, 100, 200, 500, 1000 };
     const size_t key_len = 5;
     const size_t query_count = 100000;
+    const size_t repeat_count = 10;
 
     for (size_t dict_size: dict_sizes) {
-        std::cout << "=====================================================\n";
+        //std::cout << "=====================================================\n";
         std::cout << "Benchmark with N = " << dict_size
             << ", L = " << key_len
             << ", Q = " << query_count
+            << ", R = " << repeat_count
             << std::endl;
         benchmark bm(dict_size, key_len, query_count);
 
-        // warm up
-        bm.benchmark_dict(query_count);
-
         auto time_cdict = bm.benchmark_dict(query_count);
-        std::cout << "C naive dictionary time: " << time_cdict << " us" << std::endl;
+        for (size_t i = 1; i < repeat_count; ++i) {
+            time_cdict = std::min(time_cdict, bm.benchmark_dict(query_count));
+        }
+        std::cout << "C naive dictionary time: " 
+            << time_cdict << " us" 
+            << std::endl;
 
-        // warm up
-        bm.benchmark_unordered_map(query_count);
+        auto time_rbtree = bm.benchmark_map(query_count);
+        for (size_t i = 1; i < repeat_count; i++) {
+            time_rbtree = std::min(time_rbtree, bm.benchmark_map(query_count));
+        }
+        double rbtree_speedup = static_cast<double>(time_cdict)/time_rbtree;
+        std::cout << "C++ std::map time:       " 
+            << time_rbtree << " us "
+            << "(speedup: "
+            << std::fixed
+            << std::setprecision(2)
+            << rbtree_speedup 
+            << "x)" 
+            << std::endl;
 
         auto time_hsmap = bm.benchmark_unordered_map(query_count);
-        std::cout << "C++ unordererd map time: " << time_hsmap << " us" << std::endl;
+        for (size_t i = 1; i < repeat_count; i++) {
+            time_hsmap = std::min(time_hsmap, bm.benchmark_unordered_map(query_count));
+        }
+        double hash_speedup = static_cast<double>(time_cdict)/time_hsmap;
+        std::cout << "C++ unordererd map time: " 
+            << time_hsmap << " us" 
+            << "(speedup: "
+            << std::fixed
+            << std::setprecision(2)
+            << hash_speedup 
+            << "x)" 
+            << std::endl;
+
         std::cout << "=====================================================\n";
     }
 
